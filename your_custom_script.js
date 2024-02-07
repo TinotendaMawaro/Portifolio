d87d1e2d6d4a86a53b0e880159b579af1192db80
Tinotenda Mawaro <tmawaro25@gmail.com>
2024-02-07 17:31:59 -0500

    hh

--- your_custom_script.js
+++ your_custom_script.js
@@ -1,10 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBwyQRgX7z3gU9YMlEjTuwNgLcr1K-8bkc",
@@ -15,7 +8,3 @@ const firebaseConfig = {
   appId: "1:286935376093:web:d1055e08257035f923bb34",
   measurementId: "G-G2WYF0531X"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
